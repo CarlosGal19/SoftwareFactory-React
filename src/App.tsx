9e5340a61d5f766ed305959d3741f9c21dbb0072
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-28 14:16:17 -0600

    feat: use major provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import Register from './pages/Register'
 import ForgetPassword from './pages/ForgetPassword'
 import NewPassword from './pages/NewPassword'
 import ConfirmAccount from './pages/ConfirmAccount'
+import { MajorProvider } from "./context/MajorProvider.tsx"
 
 function App() {
 
   return (
     <>
+      <MajorProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<AuthLayout />} >
             <Route index element={<Login/>}/>
-            <Route path='register' element={<Register/>} />
+              <Route path='register' element={<Register/>} />
             <Route path='forget-password' element={<ForgetPassword/>} />
             <Route path='forget-password/:token' element={<NewPassword/>} />
             <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
           </Route>
         </Routes>
       </BrowserRouter>
+      </MajorProvider>
     </>
   )
 }
