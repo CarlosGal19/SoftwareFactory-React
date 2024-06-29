213fe249de99b7a0c3c3ab70cced5cfbe6118ee6
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-29 13:59:07 -0600

    feat: create major selector component

--- src/components/Selectors/Major.tsx
+++ src/components/Selectors/Major.tsx
@@ -1,45 +1,53 @@
-import{ FC, useEffect, useState, FormEvent } from 'react'
-import axiosClient from '../../config/axios'
-import useMajor from '../../hooks/useMajor'
+import { FC, useEffect, useState, FormEvent } from 'react';
+import axiosClient from '../../config/axios';
+import useMajor from '../../hooks/useMajor';
 
 interface Majors {
-    id: number
-    name: string
-    description: string
+    id: number;
+    name: string;
+    description: string;
 }
 
 const Major: FC = () => {
-
-    const {major} = useMajor();
-    const [majors, setMajors] = useState<Majors[]>([])
+    const { major, setMajor } = useMajor();
+    const [majors, setMajors] = useState<Majors[]>([]);
 
     useEffect(() => {
-        async function fetchMajors(){
+        async function fetchMajors() {
             try {
-                const response = await axiosClient.get('/majors')
-                setMajors(response.data.majors)
+                const response = await axiosClient.get('/majors');
+                setMajors(response.data.majors);
             } catch (error: any) {
-                setMajors([])
+                setMajors([]);
             }
         }
         fetchMajors();
-    }, [])
+    }, []);
 
-    const handleChange = (e: FormEvent) => {
-        console.log(e.target)
+    const handleChange = (e: FormEvent<HTMLSelectElement>) => {
+        const value = parseInt(e.currentTarget.value);
+        setMajor(value);
+        console.log(value);
     }
 
-  return (
-    <>
-        <select name="major" id="major">
-            {
-                !majors.length ? <option value={major} onChange={handleChange}>No majors</option> : majors.map(major => (
-                    <option key={major.id} value={major.id}>{major.name}</option>
-                ))
-            }
-        </select>
-    </>
-  )
+    return (
+        <>
+            <select name="major" id="major" value={major} onChange={handleChange}>
+                {
+                    !majors.length ? (
+                        <option value="">No majors</option>
+                    ) : (
+                        <>
+                            <option value="">Choose a major</option>
+                            {majors.map(major => (
+                                <option key={major.id} value={major.id}>{major.name}</option>
+                            ))}
+                        </>
+                    )
+                }
+            </select>
+        </>
+    );
 }
 
-export default Major
+export default Major;
