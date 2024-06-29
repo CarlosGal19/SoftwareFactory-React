b14eca12ad2d0722af3b9c3ac4343878e3d54e62
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-29 14:25:31 -0600

    feat: create form validation and post new users

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,49 +1,70 @@
-import { useState, FC, FormEvent } from "react"
-import { Link } from "react-router-dom"
-import Major from "../components/Selectors/Major"
-import useMajor from "../hooks/useMajor"
-// import Alert from "../components/Alert"
+import { useState, FC, FormEvent } from "react";
+import { Link } from "react-router-dom";
+import Major from "../components/Selectors/Major";
+import useMajor from "../hooks/useMajor";
+import Alert from "../components/Alert";
+import axiosClient from "../config/axios";
 
 const Register: FC = () => {
+  const { major } = useMajor();
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [genre, setGenre] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
 
-  const {major} = useMajor();
-  const [name, setName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
-  const [birthDate, setBirthDate] = useState('')
-  const [genre, setGenre] = useState('')
-  const [password, setPassword] = useState('')
-  const [repeatPassword, setRepeatPassword] = useState('')
+  const [alert, setAlert] = useState({ type: '', msg: '' });
 
-  // const [alert, setAlert] = useState({ type: '', msg: '' })
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
-    console.log(name);
-    console.log(lastName);
-    console.log(userName);
-    console.log(email);
-    console.log(birthDate);
-    console.log(genre);
-    console.log(password);
-    console.log(repeatPassword);
-    console.log(major);
-  }
+    if ([name, lastName, userName, email, birthDate, genre, major, password, repeatPassword].includes('')) {
+      setAlert({ type: 'alert', msg: 'All fields are required' });
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      setAlert({ type: 'alert', msg: 'Passwords do not match' });
+      return;
+    }
+
+    try {
+      const response = await axiosClient.post('/users', {
+        name,
+        last_name: lastName,
+        user_name: userName,
+        email,
+        birth_date: birthDate,
+        genre,
+        major_id: major,
+        password,
+      });
+      console.log('Register response:', response);
+    } catch (error: any) {
+      console.log('Register error:', error.response.data);
+      setAlert({ msg: error.response.data.message || 'An error occurred', type: 'alert' });
+    }
+  };
 
   return (
-    <><div>
-      <h1 className="text-6xl font-bold text-black">Sign up and show the newest <span className="text-blue-700">UTMA</span> info</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-6xl font-bold text-black">
+          Sign up and show the newest <span className="text-blue-700">UTMA</span> info
+        </h1>
+      </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        {/* {alert.msg && <Alert type={alert.type} msg={alert.msg} />} */}
+        {alert.msg && <Alert type={alert.type} msg={alert.msg} />}
         <form onSubmit={handleSubmit}>
           <div className="mb-5">
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
             <input
               type="text"
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value.trim())}
               id="name"
               name="name"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
@@ -54,7 +75,7 @@ const Register: FC = () => {
             <input
               type="text"
               value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              onChange={(e) => setLastName(e.target.value.trim())}
               id="lastName"
               name="lastName"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
@@ -65,7 +86,7 @@ const Register: FC = () => {
             <input
               type="text"
               value={userName}
-              onChange={(e) => setUserName(e.target.value)}
+              onChange={(e) => setUserName(e.target.value.trim())}
               id="userName"
               name="userName"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
@@ -76,7 +97,7 @@ const Register: FC = () => {
             <input
               type="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value.trim())}
               id="email"
               name="email"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
@@ -95,7 +116,14 @@ const Register: FC = () => {
           </div>
           <div className="mb-5">
             <label htmlFor="genre" className="block text-sm font-medium text-gray-700">Gender</label>
-            <select name="genre" id="genre" value={genre} onChange={(e) => setGenre(e.target.value)} className="mt-1 p-3 w-full border border-gray-300 rounded-md">
+            <select
+              name="genre"
+              id="genre"
+              value={genre}
+              onChange={(e) => setGenre(e.target.value)}
+              className="mt-1 p-3 w-full border border-gray-300 rounded-md"
+            >
+              <option value="">Select Gender</option>
               <option value="M">Male</option>
               <option value="F">Female</option>
               <option value="O">Other</option>
@@ -110,7 +138,7 @@ const Register: FC = () => {
             <input
               type="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value.trim())}
               id="password"
               name="password"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
@@ -121,20 +149,27 @@ const Register: FC = () => {
             <input
               type="password"
               value={repeatPassword}
-              onChange={(e) => setRepeatPassword(e.target.value)}
+              onChange={(e) => setRepeatPassword(e.target.value.trim())}
               id="repeatPassword"
               name="repeatPassword"
               className="mt-1 p-3 w-full border border-gray-300 rounded-md"
             />
           </div>
-          <button type="submit" className="bg-blue-700 hover:bg-blue-800 font-bold text-white w-full py-3 rounded-md">Log in</button>
+          <button type="submit" className="bg-blue-700 hover:bg-blue-800 font-bold text-white w-full py-3 rounded-md">
+            Sign up
+          </button>
         </form>
-        <div className='flex justify-evenly'>
-          <p className="mt-5 text-center">Don't have an account? <Link to="/register" className="text-blue-700">Register</Link></p>
-          <p className="mt-5 text-center"><Link to="/forget-password" className="text-blue-700">Forgot password?</Link></p>
+        <div className="flex justify-evenly">
+          <p className="mt-5 text-center">
+            Already have an account? <Link to="/login" className="text-blue-700">Log in</Link>
+          </p>
+          <p className="mt-5 text-center">
+            <Link to="/forget-password" className="text-blue-700">Forgot password?</Link>
+          </p>
         </div>
-      </div></>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
