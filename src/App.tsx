8965a7def7d86daef21db6f762cdf708e45aca02
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-10 12:24:40 -0600

    feat: create auth provider and custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,27 @@ import ForgetPassword from './pages/ForgetPassword'
 import NewPassword from './pages/NewPassword'
 import ConfirmAccount from './pages/ConfirmAccount'
 import { MajorProvider } from "./context/MajorProvider.tsx"
+import { AuthProvider } from "./context/AuthProvider.tsx"
 
 function App() {
 
   return (
     <>
-      <MajorProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AuthLayout />} >
-            <Route index element={<Login/>}/>
-            <Route path='register' element={<Register/>} />
-            <Route path='forget-password' element={<ForgetPassword/>} />
-            <Route path='forget-password/:token' element={<NewPassword/>} />
-            <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      </MajorProvider>
+      <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <MajorProvider>
+                <Route path="/" element={<AuthLayout />} >
+                  <Route index element={<Login/>}/>
+                  <Route path='register' element={<Register/>} />
+                  <Route path='forget-password' element={<ForgetPassword/>} />
+                  <Route path='forget-password/:token' element={<NewPassword/>} />
+                  <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
+                </Route>
+              </MajorProvider>
+            </Routes>
+          </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useState } from "react";
+
+// Create context that validates if user is authenticated with a JWT
+
+interface AuthContextProps {
+    isAuthenticated: boolean;
+    login: (jwt: string) => void;
+    logout: () => void;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+const AuthContext = createContext({} as AuthContextProps);
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const login = (jwt: string) => {
+        localStorage.setItem("jwt", jwt);
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("jwt");
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export { AuthProvider, AuthContext };
+
