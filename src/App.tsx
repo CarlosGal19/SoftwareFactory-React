f50e02496358c8a1e1a21b60dae460024284db05
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-15 22:29:36 -0600

    feat: create protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AuthLayout from "./layout/AuthLayout"
-import Login from './pages/Login/Login.tsx'
-import Register from './pages/Login/Register'
-import ForgetPassword from './pages/Login/ForgetPassword'
-import NewPassword from './pages/Login/NewPassword'
-import ConfirmAccount from './pages/Login/ConfirmAccount'
-import { MajorProvider } from "./context/MajorProvider.tsx"
-import { AuthProvider } from "./context/AuthProvider.tsx"
-import HomeLayout from './layout/AuthLayout'
-import Home from './pages/Protect/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layout/AuthLayout";
+import Login from './pages/Login/Login.tsx';
+import Register from './pages/Login/Register';
+import ForgetPassword from './pages/Login/ForgetPassword';
+import NewPassword from './pages/Login/NewPassword';
+import ConfirmAccount from './pages/Login/ConfirmAccount';
+import { MajorProvider } from "./context/MajorProvider.tsx";
+import { AuthProvider } from "./context/AuthProvider.tsx";
+import Home from './pages/Protect/Home';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
 
   return (
     <>
       <AuthProvider>
-          <MajorProvider>
+        <MajorProvider>
           <BrowserRouter>
             <Routes>
-                <Route path="/" element={<AuthLayout />} >
-                  <Route index element={<Login/>}/>
-                  <Route path='register' element={<Register/>} />
-                  <Route path='forget-password' element={<ForgetPassword/>} />
-                  <Route path='forget-password/:token' element={<NewPassword/>} />
-                  <Route path='confirm-account/:id' element={<ConfirmAccount/>} />
-                </Route>
-                <Route path="/home" element={<HomeLayout />}>
-                  <Route index element={<Home />} />
-                </Route>
+              <Route path="/" element={<AuthLayout />} >
+                <Route index element={<Login />} />
+                <Route path='register' element={<Register />} />
+                <Route path='forget-password' element={<ForgetPassword />} />
+                <Route path='forget-password/:token' element={<NewPassword />} />
+                <Route path='confirm-account/:id' element={<ConfirmAccount />} />
+              </Route>
+              <Route path="/home" element={<ProtectedRoute />}>
+                <Route index element={<Home />} />
+              </Route>
             </Routes>
           </BrowserRouter>
         </MajorProvider>
@@ -36,4 +36,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
