1ee99c1c815a31dade10bf8c77707e1af85554f0
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-15 22:29:57 -0600

    feat: create protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ?
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+    :
+    <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
