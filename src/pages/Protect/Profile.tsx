2ca29dbba3c5ff67173baaf62d60ff186179865d
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-17 11:43:53 -0600

    feat: create profile route for user info

--- src/pages/Protect/Profile.tsx
+++ src/pages/Protect/Profile.tsx
@@ -1,7 +1,28 @@
 import axiosClient from "../../config/axios";
-import { useEffect } from "react";
+import { useEffect, useState, FC } from "react";
+import Alert from "../../components/Alert";
+import Major from '../../components/Descriptions/Major';
 
-const Profile = () => {
+type User = {
+    id: number;
+    name: string;
+    last_name: string;
+    email: string;
+    user_name: string;
+    user_type_id: number;
+    genre: string;
+    major_id: number;
+    birth_date: string;
+    profile_photo: string;
+    created_at: string;
+    updated_at: string;
+}
+
+
+const Profile : FC = () => {
+
+    const [user, setUser] = useState<User>({} as User);
+    const [alert, setAlert] = useState({ msg: '', type: '' });
 
     const jwt = localStorage.getItem('jwt');
 
@@ -13,19 +34,41 @@ const Profile = () => {
                         Authorization: `Bearer ${jwt}`
                     }
                 });
-                console.log(response.data.user);
-            } catch (error) {
-                console.log(error);
-            }
+                setUser(response.data.user);
+            } catch (error: any) {
+                console.error('Error:', error.response.data);
+                setAlert({ msg: error.response.data.message || 'An error occurred', type: 'alert' });
+              }
         }
         fetchProfile();
-    }, []);
+    }, [jwt]);
 
   return (
     <>
-        <div className="my-32">
-            Profile
-        </div>
+        {
+            alert.msg && <Alert msg={alert.msg} type={alert.type} />
+        }
+        {
+            user.id && (
+                <div className="mt-24">
+                    <h1 className="text-center text-6xl font-bold">Account settings</h1>
+                    <div className="grid md:grid-cols-2 grid-cols-1">
+                        <div className="my-12">
+                            <img src={user.profile_photo || './user.svg'} alt="Profile photo" className="w-72 m-auto" />
+                            <h1 className="text-center text-4xl font-bold my-16">{user.name} {user.last_name}</h1>
+                        </div>
+                        <div className="my-12">
+                            <h2 className="text-4xl font-bold my-8">Personal information</h2>
+                            <p className="text-2xl my-4">Email: {user.email}</p>
+                            <p className="text-2xl my-4">Username: {user.user_name}</p>
+                            <p className="text-2xl my-4">Gender: {user.genre}</p>
+                            <Major id={user.major_id} />
+                            <p className="text-2xl my-4">Birth date: {user.birth_date}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     </>
   )
 }
