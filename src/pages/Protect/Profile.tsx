45fbe5a9066876156efefd9e4c9e087df7558b6e
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-18 08:03:09 -0600

    refactor: call only a endpoint

--- src/pages/Protect/Profile.tsx
+++ src/pages/Protect/Profile.tsx
@@ -1,73 +1,7 @@
-import axiosClient from "../../config/axios";
-import { useEffect, useState, FC } from "react";
-import Alert from "../../components/Alert";
-import Major from '../../components/Descriptions/Major';
-
-type User = {
-    id: number;
-    name: string;
-    last_name: string;
-    email: string;
-    user_name: string;
-    user_type_id: number;
-    genre: string;
-    major_id: number;
-    birth_date: string;
-    profile_photo: string;
-    created_at: string;
-    updated_at: string;
+const Profile = () => {
+  return (
+    <div className="my-24">Profile</div>
+  )
 }
 
-const Profile: FC = () => {
-    const [user, setUser] = useState<User>({} as User);
-    const [alert, setAlert] = useState({ msg: '', type: '' });
-
-    const jwt = localStorage.getItem('jwt');
-
-    useEffect(() => {
-        const fetchProfile = async () => {
-            try {
-                const response = await axiosClient.get('users/profile', {
-                    headers: {
-                        Authorization: `Bearer ${jwt}`
-                    }
-                });
-                setUser(response.data.user);
-            } catch (error: any) {
-                console.error('Error:', error.response.data);
-                setAlert({ msg: error.response.data.message || 'An error occurred', type: 'alert' });
-            }
-        }
-        fetchProfile();
-    }, [jwt]);
-
-    return (
-        <>
-            {alert.msg && <Alert msg={alert.msg} type={alert.type} />}
-            {user.id && (
-                <div className="mt-24 p-8 max-w-4xl mx-auto bg-white rounded-lg shadow-lg">
-                    <h1 className="text-center text-6xl font-bold text-gray-800 mb-8">Account Settings</h1>
-                    <div className="grid md:grid-cols-2 gap-8">
-                        <div className="flex flex-col items-center">
-                            <img src={user.profile_photo || './user.svg'} alt="Profile" className="w-48 h-48 rounded-full object-cover mb-4 shadow-md" />
-                            <h1 className="text-center text-4xl font-bold text-gray-800">{user.name} {user.last_name}</h1>
-                        </div>
-                        <div>
-                            <h2 className="text-4xl font-bold text-gray-800 mb-4">Personal Information</h2>
-                            <div className="text-xl text-gray-700 space-y-4">
-                                <p><span className="font-semibold">Email:</span> {user.email}</p>
-                                <p><span className="font-semibold">Username:</span> {user.user_name}</p>
-                                <p><span className="font-semibold">Gender:</span> {user.genre}</p>
-                                <Major id={user.major_id} />
-                                <p><span className="font-semibold">Birth Date:</span> {user.birth_date}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </>
-    );
-}
-
-export default Profile;
-
+export default Profile
