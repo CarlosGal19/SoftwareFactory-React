2cac924b6fbe23d2173c0c8836f5efb065cf7b97
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-18 14:05:44 -0600

    feaT: show posts, forums and friends components in home

--- src/pages/Protect/Home.tsx
+++ src/pages/Protect/Home.tsx
@@ -1,9 +1,19 @@
+import Forums from '../../components/Forums';
+import Posts from '../../components/Posts';
+import Friends from '../../components/Friends';
+
 const Home = () => {
+
   return (
-    <div className="my-32">
-      Hola
+    <div className='md:flex'>
+        <Forums />
+        <div className="my-32 mx-8 w-3/5 text-center">
+          <h1 className='text-6xl font-bold mb-6'>Welcome to most recent UTMA info</h1>
+          <Posts />
+        </div>
+        <Friends />
     </div>
   )
 }
 
-export default Home
+export default Home;
