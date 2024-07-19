44e60213b9a73dce02c7a321cf8e170ab101ab95
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-19 11:37:03 -0600

    feat: show posts on home page

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -6,7 +6,7 @@ import { FC } from "react";
 type Post = {
     id: number;
     topic_id: number;
-    creatot_id: number;
+    creator_id: number;
     title: string;
     content: string;
     url_img: string;
@@ -43,10 +43,10 @@ const Posts: FC = () => {
                 posts.length > 0 ? (
                     <ul>
                         {posts.map((post: any) => (
-                            <li key={post.id} className="mt-4 p-4 bg-white shadow-lg rounded-lg text-center w-1/2 m-auto">
-                                <h2 className="text-4xl font-bold">{post.title}</h2>
-                                <h3 className="text-2xl font-bold">{post.content}</h3>
-                                <img src={post.url_img} alt="Post Image" />
+                            <li key={post.id} className="mt-4 p-4 bg-white shadow-lg rounded-lg text-center w-2/3 m-auto">
+                                <h2 className="text-4xl font-bold my-4">{post.title}</h2>
+                                <h3 className="text-2xl my-2 font-semibold">{post.content}</h3>
+                                <img src={post.url_img} alt="Post Image" className="m-auto" />
                             </li>
                         ))}
                     </ul>
