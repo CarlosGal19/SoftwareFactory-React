91cc890cdd63f12486f50198de680af4b790daa1
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-19 11:17:23 -0600

    docs: remove comments

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -7,8 +7,6 @@ import {
 import useStore from "../zustand/Zustand";
 import { useNavigate } from "react-router-dom";
 
-// Create context that validates if user is authenticated with a JWT
-
 interface AuthContextProps {
   isAuth: boolean;
   login: (jwt: string) => void;
@@ -22,7 +20,6 @@ interface AuthProviderProps {
 const AuthContext = createContext({} as AuthContextProps);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const { isAuth, login: stateLogin, logout: stateLogout } = useStore();
   const navigate = useNavigate();
@@ -43,7 +40,6 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
   useEffect(() => {
       const jwt = localStorage.getItem("jwt");
-      console.log("AuthContext useEffect", isAuth, jwt);
     if (jwt) {
       login(jwt);
       navigate("/home");
