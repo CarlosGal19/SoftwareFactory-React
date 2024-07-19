754b72bb8d3f2eb48ced82206046933024183f67
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-19 11:37:25 -0600

    feat: add type to post

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -42,7 +42,7 @@ const Posts: FC = () => {
             {
                 posts.length > 0 ? (
                     <ul>
-                        {posts.map((post: any) => (
+                        {posts.map((post: Post) => (
                             <li key={post.id} className="mt-4 p-4 bg-white shadow-lg rounded-lg text-center w-2/3 m-auto">
                                 <h2 className="text-4xl font-bold my-4">{post.title}</h2>
                                 <h3 className="text-2xl my-2 font-semibold">{post.content}</h3>
