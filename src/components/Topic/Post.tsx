52467ef0eb0d0ddb8b0bf331082e6148dafe8382
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-24 11:11:45 -0600

    feat: show creator username and time that was created

--- src/components/Topic/Post.tsx
+++ src/components/Topic/Post.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import PostCreator from "../Home/PostCreator"
 
 type Post = {
     id: number;
@@ -15,9 +16,11 @@ const Post: FC<{ post: Post }> = ({ post }) => {
     return (
         <>
             <div key={post.id} className="bg-gray-100 p-4 my-4 rounded-lg hover:shadow-blue-200 hover:cursor-pointer shadow-lg w-5/6 m-auto">
+                <PostCreator id={post.creator_id} />
                 <h2 className="text-4xl font-bold text-gray-800 my-6">{post.title}</h2>
                 <h3 className="text-2xl font-semibold text-gray-800 my-4">{post.content}</h3>
                 <img src={post.url_img} alt="Image" className="w-48" />
+                <p className="text-gray-600 my-4">Created at: {post.created_at}</p>
             </div>
         </>
     )
