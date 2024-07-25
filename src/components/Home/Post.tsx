9238bbe0f50a23afd7bdbaeee829ba0204a0286b
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-24 18:15:57 -0600

    style: edit styles from component

--- src/components/Home/Post.tsx
+++ src/components/Home/Post.tsx
@@ -14,10 +14,10 @@ type Post = {
 
 const Post: FC<{ post: Post }> = ({ post }) => {
     return (
-        <li key={post.id} className="mt-4 p-4 bg-white shadow-lg rounded-lg text-center w-2/3 m-auto">
+        <li key={post.id} className="mt-4 p-4 bg-white shadow-lg rounded-lg text-center w-5/6 m-auto">
             <PostCreator id={post.creator_id} />
             <h2 className="text-4xl font-bold my-4">{post.title}</h2>
-            <h3 className="text-2xl my-2 font-semibold">{post.content}</h3>
+            <h3 className="text-2xl my-2">{post.content}</h3>
             <img src={post.url_img} alt="Post Image" className="m-auto" />
             <h3 className="text-xl font-semibold my-2 text-left">{post.created_at}</h3>
         </li>
