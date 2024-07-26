75629649925eea8d3835a1ff11360e11928fe564
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-26 12:23:35 -0600

    style: update style from component that show friend suggestions

--- src/components/Home/Friend.tsx
+++ src/components/Home/Friend.tsx
@@ -1,21 +1,25 @@
 import { FC } from "react";
 
 type User = {
-    id: number
-    name: string
-    last_name: string,
-    profile_photo: string
-  }
+    id: number;
+    name: string;
+    last_name: string;
+    profile_photo: string;
+};
 
-const Friend: FC<{ friend : User}> = ({friend}) => {
+const Friend: FC<{ friend: User }> = ({ friend }) => {
     return (
-        <>
-            <div className='flex items-center p-2'>
-                <img src={friend.profile_photo} alt={friend.name} className='w-12 h-12 rounded-full' />
-                <p className='ml-4'>{friend.name} {friend.last_name}</p>
-            </div>
-        </>
-    )
-}
+        <div className="flex justify-between items-center">
 
-export default Friend
+            <div className='flex items-center p-2 m-auto'>
+                <img src={friend.profile_photo} alt={friend.name} className='w-12 h-12 rounded-full' />
+                <p className='ml-4 font-semibold text-sm'>{friend.name} {friend.last_name}</p>
+            </div>
+            <div className="mr-4">
+                <img src="/user_add.svg" alt="Add Friend" className='ml-4 w-6 h-6' /> {/* Usa la URL del SVG */}
+            </div>
+        </div>
+    );
+};
+
+export default Friend;
--- src/components/Home/FriendSuggestion.tsx
+++ src/components/Home/FriendSuggestion.tsx
@@ -35,7 +35,7 @@ const FriendSuggestion: FC = () => {
   return (
     <div className="mt-16 w-1/6 hidden md:block bg-teal-500">
       <h1 className='text-center font-bold text-4xl mt-4'>Friend Suggestions</h1>
-      <div>
+      <div className="mt-6">
         {
           users.map((user) => (
             <Friend key={user.id} friend={user} />
