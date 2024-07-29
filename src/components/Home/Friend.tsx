f87231549eb840a41a494a1c819a2c623f42b6d9
Carlos Galindo <utm22030587@utma.edu.mx>
2024-07-29 11:15:06 -0600

    feat: send friend request

--- src/components/Home/Friend.tsx
+++ src/components/Home/Friend.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import axiosClient from "../../config/axios";
 
 type User = {
   id: number;
@@ -8,13 +9,32 @@ type User = {
 };
 
 const Friend: FC<{ friend: User }> = ({ friend }) => {
+
+  const [sent, setSent] = useState<boolean>(false);
+
+  const jwt = localStorage.getItem('jwt');
+
+  const handleClick = async () => {
+    try {
+      const response = await axiosClient.post(`/friend-requests/${friend.id}`, {}, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      console.log(response.data);
+      setSent(true);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="flex items-center rounded-lg hover:shadow-md transition-shadow duration-200 space-x-4">
       <img src={friend.profile_photo} alt={`${friend.name} ${friend.last_name}`} className="w-12 h-12 rounded-full object-cover" />
       <div className="flex-grow">
         <h2 className="text-m font-semibold text-slate-300">{friend.name} {friend.last_name}</h2>
       </div>
-      <button className="bg-blue-500 text-slate-300 px-2 py-1 rounded-lg hover:bg-blue-600 transition-colors duration-200">
+      <button className="bg-blue-500 text-slate-300 px-2 py-1 rounded-lg hover:bg-blue-600 transition-colors duration-200" onClick={handleClick}>
         <img src="/user_add.svg" alt="Add Friend" className="w-6 h-6" />
       </button>
     </div>
