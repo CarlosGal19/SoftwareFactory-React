42d9a5cf9490b34d73919dcc4d4a597278395548
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-02 13:36:29 -0600

    feat: fetch no posts validated counter

--- src/components/AdminDashboard/Statistics/PendingPosts.tsx
+++ src/components/AdminDashboard/Statistics/PendingPosts.tsx
@@ -1,8 +1,37 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+import axiosClient from '../../../config/axios'
+import { AxiosError } from 'axios'
 
 const PendingPosts: FC = () => {
+
+  const [posts, setPosts] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(true)
+
+  const jwt = localStorage.getItem('jwt')
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axiosClient.get('/validated/no/count', {
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          }
+        })
+        setPosts(response.data.count)
+      } catch (error) {
+        const err = error as AxiosError
+        console.log(err.response?.data)
+      } finally {
+        setLoading(false)
+      }
+    }
+    getPosts();
+  }, []);
+
+  if (loading) return <p className="text-2xl">...</p>
+
   return (
-    <p className="text-2xl">23</p>
+    <p className="text-2xl">{posts}</p>
   )
 }
 
