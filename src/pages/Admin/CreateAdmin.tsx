0afffb23e4af4dbec66c8d9dd4a13643210ca6a6
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-09 13:47:43 -0600

    feat: import admins and form to create more admins

--- src/pages/Admin/CreateAdmin.tsx
+++ src/pages/Admin/CreateAdmin.tsx
@@ -1,6 +1,24 @@
-const CreateAdmin = () => {
+import { FC } from "react"
+import Admins from "../../components/Admin/Admins";
+import FormAdmin from "../../components/Admin/FormAdmin";
+
+const CreateAdmin: FC = () => {
+
   return (
-    <div>CreateAdmin</div>
+    <>
+    <div className="p-6 w-full">
+      <h1 className="text-3xl font-bold mb-6">Manage Admins</h1>
+      <div className="mt-8 bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Admins: </h2>
+        <Admins />
+      </div>
+      <div className="mt-8 bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Create admin: </h2>
+        <FormAdmin />
+      </div>
+    </div>
+    </>
+
   )
 }
 
